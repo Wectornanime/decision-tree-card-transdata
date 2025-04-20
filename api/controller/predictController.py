baa28a0eedf8f8566be5c91656e8a0cc5c6dad5c
Wectornanime <wectornanime@gmail.com>
2025-04-20 19:36:24 -0300

    implement function to save predictions

--- api/controller/predictController.py
+++ api/controller/predictController.py
@@ -3,6 +3,9 @@ import joblib
 from models.predictModel import PredictJsonModel
 from services.makePrediction import MakePrediction
 from services.transformInDataFrame import TransformDataInDataFrame
+from services.savePredictIntoDb import SavePredictIntoDb
+
+from repository.predictionRepository import PredictRepository
 
 class PredictController:
     def post(self, data: PredictJsonModel):
@@ -10,6 +13,10 @@ class PredictController:
 
         make_prediction = MakePrediction(model)
         data_frame_transform = TransformDataInDataFrame(data)
+        prediction_repository = PredictRepository('database/database.db')
+        save_predict_into_db = SavePredictIntoDb(prediction_repository)
 
         predict = make_prediction.predict(data_frame_transform.transform())
+        save_predict_into_db.save(inputData=data, outputData=predict)
+
         return predict
