ee12eac17cc00d4c08558dbb47aacbe069e279a9
Wectornanime <wectornanime@gmail.com>
2025-04-21 09:11:22 -0300

    add cors into main.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from controller.predictController import PredictController
 from models.predictModel import PredictJsonModel
 from services.databaseSetup import DatabaseSetup
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ["http://localhost:5173"] para restringir
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 predict_controller = PredictController()
 database_setup = DatabaseSetup(db='database/database.db')
